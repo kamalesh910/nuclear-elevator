6ac4095fe5fe290e72e12b10b8fe1b76234cef0a
Kamalesh Dhage <kdhage910@gmail.com>
2025-08-31 17:14:31 +0530

    UI Updated

--- frontend/src/app/components/gallery/gallery.ts
+++ frontend/src/app/components/gallery/gallery.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { DynamicGallery } from '../dynamic-gallery/dynamic-gallery';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [DynamicGallery],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css'
 })
 export class Gallery {
+  selectedSection: string = '';
+  showGallery: boolean = false;
 
+  openGallery(sectionName: string) {
+    this.selectedSection = sectionName;
+    this.showGallery = true;
+  }
+
+  onGalleryClose() {
+    this.showGallery = false;
+  }
 }
